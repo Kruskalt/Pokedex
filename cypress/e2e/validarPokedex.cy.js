cb6317039e7498f61a0c3df955d43a471cf9e675
NazarenoPortatil <codigorap13@gmail.com>
2024-01-30 14:58:30 -0300

    Estuve haciendo pruebas con cypress. Aplicando lo de fixture. cy.route y server ya no andan, ahora se usa intercept().

--- cypress/e2e/validarPokedex.cy.js
+++ cypress/e2e/validarPokedex.cy.js
@@ -1,4 +1,5 @@
-const URL = '192.168.0.49:8080'
+/// <reference types="Cypress"/>
+const URL = '192.168.0.200:8080'
 
 
 context("validarPokedex", () => {
@@ -10,12 +11,16 @@ context("validarPokedex", () => {
 
 
     it("validar que aparezcan los sprites de los pokemons en pantalla", () => {
+      
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=1",{fixture:'obtenerPagina1.json'}).as("obtenerPagina1")
       cy.wait(12000)
       cy.get(".row .col").should("have.length",20)
 
     });
     it("validar que aparezca el modal al hacer click en un pokemon", () => {
-      
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=1",{fixture:'obtenerPagina1.json'}).as("obtenerPagina1")
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon/1/",{fixture:'bulbasaur.json'}).as("obtenerBulbasaurSprite")
+
       cy.wait(1000)
       cy.get(".row .col:first").click()
 
@@ -24,7 +29,8 @@ context("validarPokedex", () => {
     });
 
     it("validar que borre los sprites actuales al cambiar  de pagina", () => {
-      
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=1",{fixture:'obtenerPagina1.json'}).as("obtenerPagina1")
+
       cy.wait(12000)
       cy.get(".next").click()
 
@@ -34,7 +40,9 @@ context("validarPokedex", () => {
 
     });
     it("validar que cambie de la primer pagina a la segunda, los sprites", () => {
-      
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=1",{fixture:'obtenerPagina1.json'}).as("obtenerPagina1")
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=20",{fixture:'obtenerPagina2.json'}).as("obtenerPagina2")
+
       cy.wait(5000)
       cy.get(".next").click()
       cy.wait(5000)
@@ -58,6 +66,8 @@ context("validarPokedex", () => {
     
 
     it("validar que cambie de numero en el dom que contiene el numero de pagina ", () => {
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=1",{fixture:'obtenerPagina1.json'}).as("obtenerPagina1")
+      cy.intercept("GET","https://pokeapi.co/api/v2/pokemon?limit=20&offset=20",{fixture:'obtenerPagina2.json'}).as("obtenerPagina2")
       
       cy.wait(5000)
       cy.get(".next").click()
